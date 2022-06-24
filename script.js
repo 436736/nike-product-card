7d8ee8032167b0961efe132d093962250d97d3a7
NABIL ADIB <nabil298adib@gmail.com>
2022-06-25 01:02:35 +0800

    Animation - Not change in transition

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = querySelector(".container");
+const color1 = querySelector(".color-1");
+const color2 = querySelector(".color-2");
+
+color2.addEventListener("click", () => {
+    container.classList(".change");
+})
